b56c0c623f3a7781cc1416d818ca485c16503ad1
Frank <frankq0509@gmail.com>
2025-02-09 22:10:28 -0500

    Update HistoriasClinicasPanel.java

--- src/ui/HistoriasClinicasPanel.java
+++ src/ui/HistoriasClinicasPanel.java
@@ -24,8 +24,8 @@ public class HistoriasClinicasPanel extends JPanel {
         JLabel logoLabel = new JLabel(logoIcon);
 
         JLabel titleLabel = new JLabel("SmartTurn");
-        titleLabel.setFont(new Font("Script MT Bold", Font.BOLD, 50)); // Tamaño de fuente más pequeño
-        titleLabel.setForeground(Color.WHITE); // Texto en blanco
+        titleLabel.setFont(new Font("Script MT Bold", Font.BOLD, 50)); 
+        titleLabel.setForeground(Color.WHITE);
 
         headerPanel.add(logoLabel);
         headerPanel.add(titleLabel);
@@ -36,9 +36,9 @@ public class HistoriasClinicasPanel extends JPanel {
         table = new JTable(tableModel);
         table.setFont(new Font("Cambria", Font.PLAIN, 25));
         table.setRowHeight(30);
-        table.setForeground(Color.WHITE); // Letras blancas
-        table.setSelectionBackground(new Color(77, 153, 77)); // Fondo helecho para selección
-        table.setSelectionForeground(Color.WHITE); // Letras blancas en selección
+        table.setForeground(Color.WHITE); 
+        table.setSelectionBackground(new Color(77, 153, 77)); 
+        table.setSelectionForeground(Color.WHITE); 
 
         table.setShowGrid(false);
         table.setIntercellSpacing(new Dimension(0, 0));
